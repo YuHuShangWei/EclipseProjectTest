0b86c4744c8333151d415200403dad15d38dc581
YuHuShangWei <14120443@bjtu.edu.cn>
2016-03-30 09:58:07 +0800

    我的这部分的功能已经实现了

---
+++ src/test2/Yuan.java
@@ -0,0 +1,19 @@
+package test2;
+
+public class Yuan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sdafasdfasdf");
+		System.out.println("埃及佛山哦你还发");
+		System.out.println("sdafasdfasdf");
+		System.out.println("sdafasdfasdf");
+		System.out.println("I发");
+		System.out.println("sdafasdfasdf");
+		System.out.println("sdafasdfasdf");
+		System.out.println("sdafasdfasdf");
+		System.out.println("sdafasdfasdf");
+		System.out.println("按法律和发挥发挥");
+	}
+
+}
